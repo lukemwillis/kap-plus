730bd5d29c48e3a14ee1a258da93b3e9344e92bc
luke <luke@prescryptive.com>
2023-03-24 14:43:46 -0700

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,44 +7,55 @@ import theme from "../styles/theme";
 import Head from "next/head";
 import { NameServiceProvider } from "../context/NameServiceProvider";
 import { ProfileProvider } from "../context/ProfileProvider";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AccountProvider>
-        <NameServiceProvider>
-          <ProfileProvider>
-            <Head>
-              <title>KAP</title>
-              <meta
-                name="viewport"
-                content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
-              />
-              <link
-                href="favicon.ico"
-                rel="icon"
-                media="(prefers-color-scheme: light)"
-              />
-              <link
-                href="favicon-inverse.ico"
-                rel="icon"
-                media="(prefers-color-scheme: dark)"
-              />
-            </Head>
-            <Flex
-              margin="auto"
-              height="100%"
-              width="100%"
-              direction="column"
-            >
-              <main style={{ flex: 1, display: "flex" }}>
-                <Component {...pageProps} />
-              </main>
-            </Flex>
-          </ProfileProvider>
-        </NameServiceProvider>
-      </AccountProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <AccountProvider>
+          <NameServiceProvider>
+            <ProfileProvider>
+              <Head>
+                <title>KAP</title>
+                <meta
+                  name="viewport"
+                  content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
+                />
+                <link
+                  href="favicon.ico"
+                  rel="icon"
+                  media="(prefers-color-scheme: light)"
+                />
+                <link
+                  href="favicon-inverse.ico"
+                  rel="icon"
+                  media="(prefers-color-scheme: dark)"
+                />
+              </Head>
+              <Flex margin="auto" height="100%" width="100%" direction="column">
+                <main style={{ flex: 1, display: "flex" }}>
+                  <Component {...pageProps} />
+                </main>
+              </Flex>
+            </ProfileProvider>
+          </NameServiceProvider>
+        </AccountProvider>
+      </ChakraProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
   );
 }
 
