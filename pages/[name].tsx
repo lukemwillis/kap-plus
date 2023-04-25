6b5bf33c8378db3fc3dd178ab2be194ab595f3ef
luke <luke@prescryptive.com>
2023-04-25 13:24:50 -0700

    hex metadata

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -81,10 +81,8 @@ const Profile: NextPage = () => {
           const { result: nftResult } = await nftContract!.functions.uri({});
 
           if (nftResult?.value) {
-            const buffer = utils.toUint8Array(profileResult.avatar_token_id);
-            const tokenId = new TextDecoder().decode(buffer);
             const uri = normalizeIpfsUris(nftResult.value as string);
-            const metadata = await fetch(`${uri}/${tokenId}`);
+            const metadata = await fetch(`${uri}/${profileResult.avatar_token_id}`);
             const { image } = await metadata.json();
             const imageSrc = normalizeIpfsUris(image);
             setAvatarSrc(imageSrc);
